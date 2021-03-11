043145d5b5406859451d0a60778e4d01321682fc
jballaro <justin.ballaro@hotmail.ca>
2021-03-10 19:01:15 -0500

    state.h: added run arguments struct and run_state

--- fsm/src/statemachinemanager.cpp
+++ fsm/src/statemachinemanager.cpp
@@ -1,10 +1,17 @@
 #include "statemachinemanager.h"
 
+#include "util.h"
+
+//will need to include headers for each state file created.
+//ex. #include "teststate.h"
 
 StateMachineManager* StateMachineManager::__instance = nullptr;
 
 StateMachineManager::StateMachineManager()
 {
+    //Next two lines are what we did to get teststate working (for reference)
+    // TestState *ts = new TestState();
+    // Util::create_task(TestState::run_state, new RunArguments(ts, nullptr), "ReadyState", osPriorityBelowNormal1, 512);
 
     if(!__instance)
         __instance = this;
--- fsm/states/include/state.h
+++ fsm/states/include/state.h
@@ -3,18 +3,46 @@
 
 #include "util.h"
 
+class State;
+
+struct RunArguments
+{
+    RunArguments(State *_s, void *_args)
+        : state(_s), argument(_args)
+    {
+    }
+
+    State *state;
+    void *argument;
+};
 
 class State
 {
+    friend class StateMachineManager;
 public:
-    State(const char* name, osPriority_t priority, uint32_t stack_size)
+    State()
     {}
 
     ~State() {}
 
 protected:
+    static void run_state(void *argument)  {
+        RunArguments *args = (RunArguments *)(argument);
+
+        if (!args) {
+            //error
+            return;
+        }
+
+        args->state->run(argument);
+
+        for (;;);
+    };
+
+private:
     virtual void run(void *argument);
 };
+
 
 
 
